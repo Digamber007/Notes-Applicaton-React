6124d591645d5ef50b8682f535cf665dbd3f9214
Digamber007 <digambertoppo007@gmail.com>
2022-09-19 19:15:41 +0530

    Notes-application-react

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import {MdModeNight, MdLightMode} from 'react-icons/md';
+
+const Header = ({ handleToggleDarkMode }) => {
+
+    const [isActive, setActive] = useState(false)
+  return (
+    <div className="header">
+      <h1>Notes</h1>
+      <button
+        onClick={() =>
+          handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
+          
+        }
+        className="save"
+      >
+        {/* <MdModeNight>Dark Mode</MdModeNight> */}
+        {isActive?<MdLightMode onClick={()=>{
+            setActive(!isActive)}}/>:
+                <MdModeNight onClick={()=>{
+            setActive(!isActive)}} />}
+        
+      </button>
+    </div>
+  );
+};
+
+export default Header;
